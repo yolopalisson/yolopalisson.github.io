29a7dd774811cbe15769e678821549652dbfe2f0
Luyolo <44893424+yolopalisson@users.noreply.github.com>
2020-05-02 23:15:33 +0200

    Update top.js

--- js/top.js
+++ js/top.js
@@ -5,7 +5,7 @@ mybutton = document.getElementById("backtotop");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
